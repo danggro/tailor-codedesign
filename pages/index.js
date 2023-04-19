d90d8ade1bb9f107747ef622fbc0534158de6ee4
danggro <anangdwin55@gmail.com>
2023-04-19 22:13:55 +0700

    add title

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,14 @@ import SectionHero from "@/components/Hero/SectionHero";
 import NavBar from "@/components/Navigation/NavBar";
 import SectionService from "@/components/Service/SectionService";
 import SectionTestimoni from "@/components/Testimoni/SectionTestimoni";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Tailor | Make Dream Clothes</title>
+      </Head>
       <NavBar />
       <SectionHero />
       <SectionAdvantage />
